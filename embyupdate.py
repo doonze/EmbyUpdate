c371e28e887912da6eccc51f7ff5d01b66bc85ae
Justin Hopper <justin@doonze.net>
2019-01-10 02:56:29 -0600

    added logic to exit script on errors from subprocesses

--- embyupdate.py
+++ embyupdate.py
@@ -28,11 +28,11 @@ args = parser.parse_args()
 # If the user hasn't used the -c/--config command line argument this will test to see if the config file exist. If it doesn't
 # it will prompt the user to run the config tool from the command line. This only needs done once.
 if args.config == False:
-	if not os.path.isfile("config.ini"):
-		print("")
-		print("Config file doesn't exist! Likely this is your first time running the script. You MUST run the script with option -c or --config to continue. This only has to be done once.")
-		print("")
-		sys.exit()
+    if not os.path.isfile("config.ini"):
+        print("")
+        print("Config file doesn't exist! Likely this is your first time running the script. You MUST run the script with option -c or --config to continue. This only has to be done once.")
+        print("")
+        sys.exit()
 
 # Here we try python3 configparser import. If that fails it means user is running python2. So we import
 # the python2 ConfigParser instead
@@ -79,11 +79,11 @@ config.read('config.ini')
 
 # Here we pull the main config parms.
 if python == '3':
-	distro = config['DISTRO']['installdistro']
-	installbeta = config['DISTRO']['releaseversion']
-	serverstop = config['SERVER']['stopserver']
-	serverstart = config['SERVER']['startserver']
-	appupdate = config['EmbyUpdate']['autoupdate']
+    distro = config['DISTRO']['installdistro']
+    installbeta = config['DISTRO']['releaseversion']
+    serverstop = config['SERVER']['stopserver']
+    serverstart = config['SERVER']['startserver']
+    appupdate = config['EmbyUpdate']['autoupdate']
 elif python == '2':
     distro = config.get('DISTRO', 'installdistro')
     installbeta = config.get('DISTRO', 'releaseversion')
@@ -93,8 +93,8 @@ elif python == '2':
 
 # This is a simple timestamp function, created so each call would have a current timestamp
 def timestamp():
-	ts = time.strftime("%x %X", time.localtime())
-	return ("<" + ts + "> ")
+    ts = time.strftime("%x %X", time.localtime())
+    return ("<" + ts + "> ")
 
 # The github API of releases for Emby Media Browser. This includes beta and production releases
 url = "https://api.github.com/repos/mediabrowser/Emby.releases/releases"
@@ -123,9 +123,9 @@ try:
             print("Couldn't determine release requested, value is " + installbeta)
 
 except Exception as e:
-	print(timestamp() + "EmbyUpdate: We didn't get an expected response from the github api, script is exiting!")
-	print(timestamp() + "EmbyUpdate: Here's the error we got -- " + str(e))
-	sys.exit()
+        print(timestamp() + "EmbyUpdate: We didn't get an expected response from the github api, script is exiting!")
+        print(timestamp() + "EmbyUpdate: Here's the error we got -- " + str(e))
+        sys.exit()
 
 ##########################################################################################################
 # This block is just setting up the variables for your selected distro. These can be updated as needed.  #
@@ -133,63 +133,63 @@ except Exception as e:
 
 # Debian/Ubuntu/Mint amd64 *************
 if distro == "Debian X64":
-	downloadurl = "wget -q --show-progress https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-deb_" + onlineversion + "_amd64.deb" 
-	installfile = "dpkg -i -E emby-server-deb_" + onlineversion + "_amd64.deb"
-	updatefile  = "emby-server-deb_" + onlineversion + "_amd64.deb"
+    downloadurl = "wget -q --show-progress https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-deb_" + onlineversion + "_amd64.deb" 
+    installfile = "dpkg -i -E emby-server-deb_" + onlineversion + "_amd64.deb"
+    updatefile  = "emby-server-deb_" + onlineversion + "_amd64.deb"
 #***************************************
 
 # Debian/Ubuntu/Mint armhf *************
 if distro == "Debian ARM":
-	downloadurl = "wget -q --show-progress https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-deb_" + onlineversion + "_armhf.deb"
-	installfile = "dpkg -i emby-server-deb_" + onlineversion + "_armhf.deb"
-	updatefile  = "emby-server-deb_" + onlineversion + "_armhf.deb"
+    downloadurl = "wget -q --show-progress https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-deb_" + onlineversion + "_armhf.deb"
+    installfile = "dpkg -i emby-server-deb_" + onlineversion + "_armhf.deb"
+    updatefile  = "emby-server-deb_" + onlineversion + "_armhf.deb"
 #***************************************
 
 # Arch Linux ***************************
 if distro == "Arch":
-	downloadurl = "notused"
-	installfile = "pacman -S emby-server"
-	updatefile  = "notused"
+    downloadurl = "notused"
+    installfile = "pacman -S emby-server"
+    updatefile  = "notused"
 #***************************************
 
 # CentOS X64 ***************************
 # In Cent I think yum will handle the stop/start of the server, but change below if needed
 if distro == "CentOS":
-	downloadurl = "yum install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
-	installfile = "notused"
-	updatefile  = "notused"
+   downloadurl = "yum install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
+   installfile = "notused"
+   updatefile  = "notused"
 #****************************************
 
 # Fedora X64 ****************************
 # Pretty sure dnf will stop/start the server, but change below if needed
 if distro == "Fedora X64":
-	downloadurl = "dnf install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
-	installfile = "notused"
-	updatefile  = "notused"
+    downloadurl = "dnf install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
+    installfile = "notused"
+    updatefile  = "notused"
 #***************************************
 
 # Fedora Armv7hl ***********************
 # Pretty sure dnf will stop/start the server, but change delow if needed
 if distro == "Fedora ARM":
-	downloadurl = "dnf install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_armv7hl.rpm"
-	installfile = "notused"
-	updatefile  = "notused"
+    downloadurl = "dnf install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_armv7hl.rpm"
+    installfile = "notused"
+    updatefile  = "notused"
 #***************************************
 
 # OpenSUSE X64 *************************
 # Pretty sure zypper will stop/start the server, but change below as needed
 if distro == "OpenSUSE X64":
-	downloadurl = "zypper install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
-	installfile = "notused"
-	updatefile  = "notused"
+    downloadurl = "zypper install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_x86_64.rpm"
+    installfile = "notused"
+    updatefile  = "notused"
 #***************************************
 
 # OpenSUSE Armv7hl *********************
 # Pretty sure zypper will stop/start the server, but change below as needed
 if distro == "OpenSUSE ARM":
-	downloadurl = "zypper install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_armv7hl.rpm"
-	installfile = "notused"
-	updatefile  = "notused"
+    downloadurl = "zypper install https://github.com/MediaBrowser/Emby.Releases/releases/download/" + onlineversion + "/emby-server-rpm_" + onlineversion + "_armv7hl.rpm"
+    installfile = "notused"
+    updatefile  = "notused"
 #**************************************
 
 ###################################################################################################
@@ -198,11 +198,11 @@ if distro == "OpenSUSE ARM":
 
 # Now were going to pull the version from the config file
 if python == '3':
-	#This fires if user is running python3
-	fileversion = config['SERVER']['embyversion']
+    #This fires if user is running python3
+    fileversion = config['SERVER']['embyversion']
 elif python == '2':
-	# This fires if user is running python2
-	fileversion = config.get('SERVER', 'embyversion')
+    # This fires if user is running python2
+    fileversion = config.get('SERVER', 'embyversion')
 
 # Ok, we've got all the info we need. Now we'll test if we even need to update or not.
 
@@ -220,20 +220,28 @@ else:
     try:
         # This will stop the server on a systemd distro if it's been set to true above
         if serverstop is True:
-            subprocess.call("systemctl stop emby-server",shell=True)
+            stopreturn = subprocess.call("systemctl stop emby-server",shell=True)
             for i in xrange(10,0,-1):
                 sys.stdout.write(str(i)+' ')
                 sys.stdout.flush()
                 time.sleep(1)
 
+            if stopreturn > 0:
+                print("Server Stop failed! Non-critical error! Investigate if needed.")
+            
         # Here we download the package to install if used
         if "notused" not in downloadurl:
-            subprocess.call(downloadurl,shell=True)
+            downreturn = subprocess.call(downloadurl,shell=True)
+            if downreturn > 0:
+                print("Download failed!! Exiting!")
+                sys.exit()
 
         # Next we install it if used
         if "notused" not in installfile:
-            #os.system(installfile)
-            subprocess.call(installfile,shell=True)
+            installreturn = subprocess.call(installfile,shell=True)
+            if installreturn > 0:
+                print("Install failed! Exiting!")
+                sys.exit()
 
         # And to keep things nice and clean, we remove the downloaded file once installed if needed
         if "notused" not in updatefile:
@@ -241,9 +249,11 @@ else:
 
         # This will restart the server if using systemd if set to True above
         if serverstart is True:
-            subprocess.call("systemctl start emby-server",shell=True)
-
-	    # Lastly we write the newly installed version into the config file
+            startreturn = subprocess.call("systemctl start emby-server",shell=True)
+            if startreturn > 0:
+                print("Server start failed. Non-critical to update but server may not be running. Investigate.")
+                
+            # Lastly we write the newly installed version into the config file
         try:
             config['SERVER']['embyversion'] = onlinefileversion
         except AttributeError:
