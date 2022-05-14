3aa502f516d073f277a967926c626d5fe91f7923
Justin Hopper <doonze@gmail.com>
2022-05-14 08:06:29 -0500

    pep

--- db/editconfig.py
+++ db/editconfig.py
@@ -3,6 +3,7 @@ Used to edit the database configs
 """
 from functions.colors import Terminalcolors as c
 
+
 def edit_config(key_map, dataclass):
     """
     The edit_config function allows the user to edit a specific attribute of a dataclass
@@ -12,12 +13,12 @@ def edit_config(key_map, dataclass):
     The function loops until the user enters 'c' or 'C'. If they enter anything else, it checks 
     to see if it's an integer between 1 and len(key_map). If so, then it prompts for new input for 
     the specified attribute.
-    
+
     Args:
         key_map: Map the user input to a specific attribute of the dataclass
         dataclass: Instantiate the class
     """
-    
+
     while True:
         print()
         response = input("Enter a line number to edit, or c to cancel: ")
@@ -29,14 +30,13 @@ def edit_config(key_map, dataclass):
             dataclass.__setattr__(key_map[response], update)
             dataclass.update_db()
             print()
-            print(f"{c.fg.lt_cyan}{key_map[response]}{c.end} value has been updated!")
+            print(
+                f"{c.fg.lt_cyan}{key_map[response]}{c.end} value has been updated!")
             dataclass.print_me()
             continue
-            
+
         if response in ("c", "C"):
             return None
-        
+
         print()
         print(f"{response} {c.fg.lt_red} is Invalid!{c.end} Please try again...")
-    
-        
