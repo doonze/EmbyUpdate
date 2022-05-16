1dd10f61e50baba2680587539eee91b8086bac47
Justin Hopper <doonze@gmail.com>
2022-05-16 04:08:15 -0500

    Mainly just a pep commit
    But I'm also testing longer line commits. I'm not sure how this works
    or how it displays, so I'm trying to test it out with this long ass
    commit message.

--- db/editconfig.py
+++ db/editconfig.py
@@ -1,9 +1,10 @@
 """
 Used to edit the database configs
 """
+from dataclasses import fields
 from db.dbobjects import DistroConfig
 from functions.colors import Terminalcolors as c
-from dataclasses import fields
+
 
 
 def edit_config(key_map, dataclass):
@@ -43,6 +44,7 @@ def edit_config(key_map, dataclass):
         print()
         print(f"{response} {c.fg.lt_red} is Invalid!{c.end} Please try again...")
 
+
 def edit_distroconfig(distro_dict):
     """
     The edit_config function allows the user to edit a specific attribute of a dataclass
@@ -63,16 +65,16 @@ def edit_distroconfig(distro_dict):
         print()
         print("Here are the distros you can edit:")
         print()
-        for i, key in enumerate(distro_dict.keys(),start=1):
+        for i, key in enumerate(distro_dict.keys(), start=1):
             key_map[str(i)] = key
             print(f"[{c.fg.orange}{i}{c.end}] {c.fg.lt_blue}{key :<16}{c.end}")
-                  
+
         print()
         response = input(f"Enter a distro number to edit it, ({c.fg.orange}a{c.end}) "
                          f"to add one, or ({c.fg.orange}c{c.end}) to cancel: ")
 
         if response in key_map.keys():
-            
+
             print()
             print(f"{c.fg.yellow}{key_map[response]}:{c.end}")
             distroconfig: DistroConfig = distro_dict[key_map[response]]
@@ -82,35 +84,36 @@ def edit_distroconfig(distro_dict):
                 if field.name is not ("distro"):
                     sub_map[str(i)] = field.name
                     print(f"[{c.fg.orange}{i}{c.end}] {c.fg.lt_blue}{field.name :<16}{c.end}: "
-                        f"{c.fg.lt_cyan}{getattr(distroconfig, field.name)}{c.end}")
-            
+                          f"{c.fg.lt_cyan}{getattr(distroconfig, field.name)}{c.end}")
+
             print()
-            
+
             response = input(f"Select a number to edit that config item, or ({c.fg.orange}c{c.end}) "
                              f"to cancel: ")
-            
+
             if response in sub_map:
                 update = input(f"{c.fg.blue}{sub_map[response]}{c.end}: The current value is "
-                       f"({c.fg.lt_cyan}{getattr(distroconfig, sub_map[response])}{c.end}). "
-                       f"What would you like to update it to? : ")
-            
+                               f"({c.fg.lt_cyan}{getattr(distroconfig, sub_map[response])}{c.end}). "
+                               f"What would you like to update it to? : ")
+
                 distroconfig.__setattr__(sub_map[response], update)
                 distroconfig.update_db()
-                
+
                 print()
-                print(f"{c.fg.lt_cyan}{sub_map[response]}{c.end} value has been updated!")
-                
+                print(
+                    f"{c.fg.lt_cyan}{sub_map[response]}{c.end} value has been updated!")
+
                 distroconfig.print_me(single=True)
-                
+
                 print()
                 response = input("Would you like to update another? [y/N]: ")
-                
+
                 if response in ('y', 'Y'):
                     continue
-                
+
                 if response in ('n', 'N'):
                     return None
-                
+
         if response in ("c", "C"):
             return None
 
