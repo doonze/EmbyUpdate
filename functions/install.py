662a7c90a4f7c950bb70da10cb8250f8529aaa5c
Justin Hopper <doonze@gmail.com>
2023-10-13 10:30:10 -0500

    added timestamps, amaended DB timestamps to non-print format, PEP updates

--- functions/install.py
+++ functions/install.py
@@ -12,6 +12,7 @@ from genericpath import exists
 import db.dbobjects as db
 from functions import exceptrace, timestamp
 
+
 # pylint: disable=broad-except
 # pylint: disable=line-too-long
 # pylint: disable=too-many-statements
@@ -19,7 +20,7 @@ from functions import exceptrace, timestamp
 
 def update_emby(configobj: db.ConfigObj):
     """
-    The update_emby function is the main function of this script. It's purpose is to update Emby Media Server
+    The update_emby function is the main function of this script. Its purpose is to update Emby Media Server
     to a newer version if available. The function will check for an updated package and install it if needed, 
     and then restart the server after updating if needed.
 
@@ -31,82 +32,81 @@ def update_emby(configobj: db.ConfigObj):
     """
 
     try:
-        distroconfig: db.DistroConfig = db.DistroConfig()
-        distroconfig.pull_from_db(configobj.mainconfig.distro)
-        install_file = distroconfig.installfile.format(online_version = configobj.onlineversion)
-        download_url = distroconfig.downloadurl.format(
-                online_version = configobj.onlineversion, install_file = install_file)
-        install_command = distroconfig.installcommand.format(install_file = distroconfig.installfile.
-                    format(online_version = configobj.onlineversion))
-                
+        distro_config: db.DistroConfig = db.DistroConfig()
+        distro_config.pull_from_db(configobj.mainconfig.distro)
+        install_file = distro_config.installfile.format(online_version=configobj.onlineversion)
+        download_url = distro_config.downloadurl.format(online_version=configobj.onlineversion,
+                                                        install_file=install_file)
+        install_command = distro_config.installcommand.format(install_file=distro_config.installfile.
+                                                              format(online_version=configobj.onlineversion))
+
         # This will stop the server if it's been set to true
         if configobj.mainconfig.stopserver:
-            
+
             print()
-            print("Stopping Emby server.....")
-            
-            stopreturn = subprocess.call("sudo service emby-server stop", shell=True)
-            
+            print(f"{timestamp.time_stamp()} Stopping Emby server.....")
+
+            stop_return = subprocess.call("sudo service emby-server stop", shell=True)
+
             time.sleep(3)
-            
-            if stopreturn > 0:
+
+            if stop_return > 0:
                 print()
-                print("Server Stop failed! Non-critical error! Investigate if needed.")
+                print(f"{timestamp.time_stamp()} Server Stop failed! Non-critical error! Investigate if needed.")
             else:
                 print()
-                print("Emby Server Stopped...")
+                print(f"{timestamp.time_stamp()} Emby Server Stopped...")
 
         # Here we download the package to install if used
-        if "notused" not in distroconfig.downloadurl:
-            
-            print("Starting Package download... Please wait, this can take several minutes.")
-            
+        if "notused" not in distro_config.downloadurl:
+            print(f"{timestamp.time_stamp()} Starting Package download... Please wait, this can take several minutes.")
+
             download = requests.get(download_url)
-            
-            with open(install_file,'wb') as file:
+
+            with open(install_file, 'wb') as file:
                 file.write(download.content)
-                
-            print("Package downloaded!")
+
+            print(f" {timestamp.time_stamp()} Package downloaded!")
 
         # Next we install it if used
-        if "notused" not in distroconfig.installfile:
-            
-            print("Installing/Updating Emby server....")
-            
-            installreturn = subprocess.call(install_command, shell=True)
-            
-            if installreturn > 0:
-                print("Install/Update failed! Exiting!")
+        if "notused" not in distro_config.installfile:
+
+            print(f"{timestamp.time_stamp()} Installing/Updating Emby server....")
+
+            install_return = subprocess.call(install_command, shell=True)
+
+            if install_return > 0:
+                print(f"{timestamp.time_stamp()} Install/Update failed! Exiting!")
                 sys.exit()
             else:
-                print("Install/Update Finished!")
+                print(f"{timestamp.time_stamp()} Install/Update Finished!")
 
         # And to keep things nice and clean, we remove the downloaded file once installed if needed
-        if "notused" not in distroconfig.installfile:
+        if "notused" not in distro_config.installfile:
 
             if exists(install_file):
-                print("Removing install file...")
-                
+                print(f"{timestamp.time_stamp()}Removing install file...")
+
                 os.remove(install_file)
-                
-                print("File removed!")
+
+                print(f"{timestamp.time_stamp()}File removed!")
 
         # This will restart the server if set to True
         if configobj.mainconfig.stopserver:
-            print("Restarting Emby server after update...")
-            startreturn = subprocess.call("sudo service emby-server start", shell=True)
-            if startreturn > 0:
-                print(
-                    "Server start failed. Non-critical to update but server may not be running. Investigate.")
+            print(f"{timestamp.time_stamp()} Restarting Emby server after update...")
+            start_return = subprocess.call("sudo service emby-server start", shell=True)
+            if start_return > 0:
+                print(f"{timestamp.time_stamp()} Server start failed. Non-critical to update but server may not be "
+                      f"running. Investigate.")
             else:
-                print("Server restarted!")
+                print(f"{timestamp.time_stamp()} Server restarted!")
 
         # Lastly we write the newly installed version into the config file
         try:
             configobj.mainconfig.version = configobj.onlineversion
-            configobj.mainconfig.dateupdated = timestamp.time_stamp()
-            db.MainUpdateHistory(date=timestamp.time_stamp(), version=configobj.onlineversion,
-                                 success=1, errorid="No Errors").insert_to_db()
+            configobj.mainconfig.dateupdated = timestamp.time_stamp(False)
+            db.MainUpdateHistory(date=timestamp.time_stamp(False), version=configobj.onlineversion,
+                                 success=True, errorid=0).insert_to_db()
             configobj.mainconfig.update_db()
 
             print(f"{timestamp.time_stamp()} EmbyUpdate: Updated to Emby version {configobj.mainconfig.version}."
