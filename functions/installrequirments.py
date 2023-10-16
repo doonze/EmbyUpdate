02fd241fb972292b7972bedb87b0130edfa853d1
Justin Hopper <doonze@gmail.com>
2023-10-16 00:46:33 -0500

    Removed a testing print statement

--- functions/installrequirments.py
+++ functions/installrequirments.py
@@ -11,4 +11,3 @@ def install_req():
     packages = ['requests']
     [subprocess.check_call([sys.executable, '-m', 'pip', 'install', pkg])
      for pkg in packages if not importlib.metadata.distribution(pkg)]
-    print("done")
