17da81578fc82d5657710af5d360f2dec185f5cf
Justin Hopper <doonze@gmail.com>
2023-10-15 23:48:41 -0500

    Updated exceptrace to log to the errors log

--- functions/exceptrace.py
+++ functions/exceptrace.py
@@ -2,9 +2,11 @@
 module for exception handling
 """
 import traceback
+from db.dbobjects import Errors
+from functions import timestamp as ts
 
 
-def execpt_trace(string, tracebk):
+def execpt_trace(string: str, tracebk, process: str):
     """
     The execptTrace function takes a string and traceback object as arguments.
     It prints the string, then for each line in the traceback, it prints:
@@ -15,9 +17,12 @@ def execpt_trace(string, tracebk):
     Args:
         string: Print the error message
         tracebk: Get the traceback of the exception
+        process: Emby or Self
     """
+
     ex_type, value, ex_traceback = tracebk
     print(string)
     print()
     for trace in traceback.format_exception(ex_type, value, ex_traceback):
         print(trace)
+    Errors(date=f'{ts.time_stamp(False)}', message=f'{ex_type} - {value} - {string}', mainorself=process).insert_to_db()
